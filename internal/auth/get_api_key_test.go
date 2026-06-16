fdcfa6416f1b1328de983bbbf2ad2899f546c544
Will <bootdev@fun404.com>
2026-06-15 19:25:17 -0600

    fixing test

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -8,7 +8,7 @@ import (
 func TestGetAPIKey(t *testing.T) {
 	h := http.Header{}
 	_, err := GetAPIKey(h)
-	if err == ErrNoAuthHeaderIncluded {
+	if err != ErrNoAuthHeaderIncluded {
 		t.Errorf("should return error no auth header included")
 	}
 }
